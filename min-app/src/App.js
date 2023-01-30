a6f809f791540dc215f3565757e53b7ba5262141
Thomas Kirkeng <thomas.kirkeng@gmail.com>
2023-01-30 12:55:14 +0100

    La til filene tror jeg

---
+++ min-app/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import React from 'react';
+import MapContainer from './mapContainer';
+
+function App() {
+  return (
+
+    <div className="App">
+      <h1 className="App-header">Test</h1>
+      <MapContainer />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ min-app/src/mapContainer.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect} from 'react'
+
+function MapContainer() {
+    const [map, setMap] = useState(null);
+
+    useEffect(() => {
+        const map = new window.google.maps.Map(document.getElementById('map'), {
+            zoom: 11,
+            center: {lat: 59.4121414, lng: 9.0366312},
+        });
+
+        setMap(map);
+    }, []);
+
+    return (
+        <div className="map-container">
+            <div id="map" style={{ height: '100vh', width: '100%'}} />
+        </div>
+    )
+
+}
+
+export default MapContainer;

04f74b688655ffa2aec28cc847f5857b8423b967
andreas-mk <amk.andreas.98@gmail.com>
2023-02-06 09:33:11 +0100

    Skal fungere med database nå

--- min-app/src/App.js
+++ min-app/src/App.js
@@ -1,24 +1,6 @@
 import './App.css';
-import React from 'react';
-import MapContainer from './mapContainer';
-import { initializeApp } from 'firebase/app';
-import { getFirestore, collection, getDocs } from 'firebase/firestore';
+import './Firebase.js';
 
-
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBUkxpITh4XGVD573zVXnQVRPUwJ25b89k",
-  authDomain: "prosjektoppgave23.firebaseapp.com",
-  databaseURL: "https://prosjektoppgave23-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "prosjektoppgave23",
-  storageBucket: "prosjektoppgave23.appspot.com",
-  messagingSenderId: "593962167470",
-  appId: "1:593962167470:web:36005107b44911dcf9513f",
-  measurementId: "G-S5KC45PN36"
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
 
 /* Get a list of cars from your database
 async function getBiler(db) {
